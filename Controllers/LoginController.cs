ba155a6dc3c38dd219ab7f131a39f09db93bd612
yasuo <pambox+github@gmail.com>
2019-06-10 23:17:02 +0900

    プロジェクト ファイルを追加します。

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TodoApp.Models;
+
+namespace TodoApp.Controllers
+{
+    [AllowAnonymous]
+    public class LoginController : Controller
+    {
+        readonly CustomMembershipProvider membershiProvider = new CustomMembershipProvider();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
